7704789aa1ffd811bb237cda82866311f91f34f7
Nathan <natepsacc@gmail.com>
2023-07-08 22:40:00 -0400

    balls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function Timer() {
+
+}
 
 function App() {
+
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const start = Date.now();
+    const intervalId = setInterval(() => {
+      const delta = Date.now() - start;
+      const elapsedSeconds = (Math.floor(delta / 1000) );
+      setSeconds(elapsedSeconds);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>you have spent</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {seconds / 86400}% ({seconds} seconds)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          of your day here.
+        </p>
+        <p>
+          if you make <d5>$50,000</d5> a year thats
+        </p>
+        <p>
+         <d5> ${(seconds/ 86400) * 136.986}</d5>
+        </p>
+        <p>
+          <d6>(after NYC taxes:
+        <d5>${((seconds/ 86400) * 136.986) * 0.77}</d5>)</d6>
+        </p>
+    
       </header>
     </div>
   );
 }
+
 
 export default App;
