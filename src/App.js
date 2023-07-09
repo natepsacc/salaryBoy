d5b33c279edc5b90f20b88123abba8745814f80e
Nathan <natepsacc@gmail.com>
2023-07-08 23:02:30 -0400

    balls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import NumberPicker from "react-widgets/NumberPicker";
 
 function Timer() {
 
